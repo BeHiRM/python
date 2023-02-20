313eeda590cb8986281c7f326bf8b70324246403
BehiRm <behzad_rahimi@icloud.com>
2023-02-20 18:31:57 +0330

    pic&finish

--- potential.py
+++ potential.py
@@ -20,3 +20,28 @@ for i in range(n):
     y0 = stdio.readFloat()
     q0 = stdio.readFloat()
     charges[i] = Charge(x0, y0, q0)
+    # Create a Picture depicting potential values.
+pic = Picture()
+for col in range(pic.width()):
+    for row in range(pic.height()):
+        # Compute pixel color.
+        x = 1.0 * col / pic.width()
+        y = 1.0 * row / pic.height()
+        v = 0.0
+
+        for i in range(n):
+            v += charges[i].potentialAt(x, y)    
+        v = (MAX_GRAY_SCALE / 2.0)  + (v / 2.0e10)
+        if v < 0:
+            grayScale = 0
+        elif v > MAX_GRAY_SCALE:
+            grayScale = MAX_GRAY_SCALE
+        else:
+            grayScale = int(v)            
+        color = Color(grayScale, grayScale, grayScale)
+        pic.set(col, pic.height()-1-row, color)
+
+# Draw the Picture.
+stddraw.setCanvasSize(pic.width(), pic.height())
+stddraw.picture(pic)
+stddraw.show()
