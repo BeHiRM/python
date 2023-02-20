05f2e17fbe0db7149cbbf71dd698de76524df384
BehiRm <behzad_rahimi@icloud.com>
2023-02-20 18:40:13 +0330

    last update

--- charge.py
+++ charge.py
@@ -2,7 +2,8 @@ import sys
 import math
 import stdio
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+
 
 class Charge:
 
@@ -18,9 +19,40 @@ class Charge:
         dx = x - self._rx
         dy = y - self._ry
         r = math.sqrt(dx*dx + dy*dy)
-        if r == 0.0: # Avoid division by 0
+        if r == 0.0:  # Avoid division by 0
             if self._q >= 0.0:
                 return float('inf')
             else:
                 return float('-inf')
         return COULOMB * self._q / r
+        # Return a string representation of self.
+
+    def __str__(self):
+        result = str(self._q) + ' at ('
+        result += str(self._rx) + ', ' + str(self._ry) + ')'
+        return result
+
+# -----------------------------------------------------------------------
+
+# For testing.
+# Accept floats x and y as command-line arguments. Create a Charge
+# objects with fixed position and electrical charge, and write to
+# standard output the potential at (x, y) due to the charge.
+
+
+def main():
+    x = float(sys.argv[1])
+    y = float(sys.argv[2])
+    c = Charge(.51, .63, 21.3)
+    stdio.writeln(c)
+    stdio.writeln(c.potentialAt(x, y))
+
+
+if __name__ == '__main__':
+    main()
+
+# -----------------------------------------------------------------------
+# if you test app do look like line 52,53,54
+# python charge.py .5 .5
+# 21.3 at (0.51, 0.63)
+# 1468638248194.164
